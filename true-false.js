e13e7839ae08b24329a6f65fc85cc7f65249fea8
MHShayek <mahedihassanshayek@gmail.com>
2021-02-02 12:19:29 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,19 @@
+// falsy value
+// false
+// 0
+// ""
+// undefined
+// null
+// NaN
+// truthy value
+// '0', ' ',[],{}
+
+
+
+let name=0;
+if( name || name ==0){
+    console.log("Condition is true.");
+}
+else{
+    console.log("Condition is false.");
+}
