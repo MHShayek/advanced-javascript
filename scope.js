06d0a8732851162f812c92505ef95489c258e6bd
MHShayek <mahedihassanshayek@gmail.com>
2021-02-02 20:54:15 +0600

    final commit

---
+++ scope.js
@@ -0,0 +1,22 @@
+let bonus =20;
+
+function sum(first, second){
+    let result = first + second + bonus;
+    // console.log(bonus);
+    if(result > 9){
+        var mood ="Happy";
+        mood = "funky";
+        mood = "fishy"
+        mood = "cranky"
+        // console.log(mood);
+    }
+    
+    console.log(day);
+    let day = "Friday";
+   
+    return result;
+}
+// console.log(mood);
+const output = sum(3,7);
+// console.log(bonus);
+// console.log(output);
